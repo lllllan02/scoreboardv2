3b6d45d291471eb0d1b9c0904c7314e5e4dec777
lllllan <342310798@qq.com>
2025-05-14 16:45:42 +0800

    ✨ feat: 更新清理逻辑以排除未来比赛
    在 Clean 函数中，增加了对比赛开始时间的检查，确保在清理比赛列表时，排除未来的比赛。此修改提升了数据的准确性和有效性。

--- cmd/crawler/helper/walk.go
+++ cmd/crawler/helper/walk.go
@@ -1,16 +1,17 @@
 package helper
 
 import (
+	"time"
+
 	"github.com/lllllan02/scoreboardv2/internal/model"
 )
-
 
 // Clean 清理空的比赛
 func Clean(contestList model.ContestList) {
 	for _, yearGroup := range contestList.Camp {
 		for _, contestGroup := range yearGroup {
 			for key, contest := range contestGroup {
-				if contest.BoardLink == "" {
+				if contest.BoardLink == "" || contest.Config.StartTime > time.Now().Unix() {
 					delete(contestGroup, key)
 				}
 			}
@@ -19,7 +20,7 @@ func Clean(contestList model.ContestList) {
 
 	for _, contestGroup := range contestList.ProvincialContest {
 		for key, contest := range contestGroup {
-			if contest.BoardLink == "" {
+			if contest.BoardLink == "" || contest.Config.StartTime > time.Now().Unix() {
 				delete(contestGroup, key)
 			}
 		}
@@ -27,7 +28,7 @@ func Clean(contestList model.ContestList) {
 
 	for _, contestGroup := range contestList.CCPC {
 		for key, contest := range contestGroup {
-			if contest.BoardLink == "" {
+			if contest.BoardLink == "" || contest.Config.StartTime > time.Now().Unix() {
 				delete(contestGroup, key)
 			}
 		}
@@ -35,7 +36,7 @@ func Clean(contestList model.ContestList) {
 
 	for _, contestGroup := range contestList.ICPC {
 		for key, contest := range contestGroup {
-			if contest.BoardLink == "" {
+			if contest.BoardLink == "" || contest.Config.StartTime > time.Now().Unix() {
 				delete(contestGroup, key)
 			}
 		}
